b85c76558c9ee3d0fdfe2ab8c1af07b3262a6fe2
Nitelayyo <nitelayyo@gmail.com>
2024-05-30 21:31:26 +0300

    second

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest;
+
+use serde::{Serialize, Deserialize};
+use serde_json::{to_string, from_str};
+
+#[tokio::main] // дозволяє асинхронність
+async fn main() {
+    let resp = match reqwest::get("https://httpbin.org/ip").await {
+        Ok(resp) => resp.text().await.unwrap(),
+        Err(err) => panic!("Error: {}", err)
+    };
+    println!("{}", resp)
 }
