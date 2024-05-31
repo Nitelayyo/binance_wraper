36143c99e447d0a12fc28f610f2091a493b23249
Nitelayyo <nitelayyo@gmail.com>
2024-05-31 17:20:42 +0300

    third

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,11 @@ use serde_json::{to_string, from_str};
 
 #[tokio::main] // дозволяє асинхронність
 async fn main() {
-    let resp = match reqwest::get("https://httpbin.org/ip").await {
+    let resp = match reqwest::get("https://api.binance.com/sapi/v1/convert/exchangeInfo").await {
         Ok(resp) => resp.text().await.unwrap(),
         Err(err) => panic!("Error: {}", err)
     };
     println!("{}", resp)
 }
+
+//GET /api/v1/exchangeInfo
